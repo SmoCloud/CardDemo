eb2b2ff944b112b62b8eb65989cfb03fc718572e
SmoCloud <psn.power@yahoo.com>
2025-03-05 13:52:15 -0500

    Copilot did it for me.

--- playing_card.py
+++ playing_card.py
@@ -1,3 +1,5 @@
+import random
+
 class PlayingCard:
     """A playing card class"""
     
@@ -19,3 +21,62 @@ class PlayingCard:
         if not isinstance(other, PlayingCard):
             return False
         return self.rank == other.rank and self.suit == other.suit
+
+class Deck:
+    """A deck of playing cards consisting of PlayingCard objects"""
+
+    def __init__(self):
+        self.cards = [PlayingCard(rank, suit) for suit in PlayingCard.SUITS for rank in PlayingCard.RANKS]
+
+    def shuffle(self):
+        """Shuffle the deck of cards"""
+        random.shuffle(self.cards)
+
+    def draw(self, num=1):
+        """Draw a number of cards from the deck"""
+        if num > len(self.cards):
+            raise ValueError("Not enough cards in the deck to draw")
+        drawn_cards = self.cards[:num]
+        self.cards = self.cards[num:]
+        return drawn_cards
+
+    def __len__(self):
+        """Return the number of cards remaining in the deck"""
+        return len(self.cards)
+
+class Hand:
+    """A hand of playing cards"""
+
+    def __init__(self):
+        self.cards = []
+
+    def draw_random_card(self, deck):
+        """Draw a random card from the deck"""
+        if len(deck) == 0:
+            raise ValueError("No cards left in the deck to draw")
+        random_card = random.choice(deck.cards)
+        deck.cards.remove(random_card)
+        self.cards.append(random_card)
+
+    def view_cards(self):
+        """View all the cards in the hand"""
+        return [str(card) for card in self.cards]
+
+    def __len__(self):
+        """Return the number of cards in the hand"""
+        return len(self.cards)
+
+def main():
+    deck = Deck()
+    deck.shuffle()
+
+    hand = Hand()
+    hand.draw_random_card(deck)  # Draw a random card from the deck
+    hand.draw_random_card(deck)  # Draw another random card
+
+    print(f"Cards in hand: {hand.view_cards()}")
+    print(f"Number of cards in hand: {len(hand)}")
+    print(f"Number of cards left in deck: {len(deck)}")
+
+if __name__ == '__main__':
+    main()
--- test_playing_card.py
+++ test_playing_card.py
@@ -1,9 +1,7 @@
 import unittest
-
-from playing_card import PlayingCard
+from playing_card import PlayingCard, Deck, Hand
 
 class TestPlayingCard(unittest.TestCase):
-
     def test_valid_card(self):
         card = PlayingCard("A", "Hearts")
         self.assertEqual(str(card), "A of Hearts")
@@ -16,15 +14,60 @@ class TestPlayingCard(unittest.TestCase):
         with self.assertRaises(ValueError):
             PlayingCard("A", "Stars")
 
-    def test_card_equality(self):
+    def test_equality(self):
         card1 = PlayingCard("10", "Diamonds")
         card2 = PlayingCard("10", "Diamonds")
+        card3 = PlayingCard("9", "Clubs")
         self.assertEqual(card1, card2)
+        self.assertNotEqual(card1, card3)
 
-    def test_card_inequality(self):
-        card1 = PlayingCard("10", "Diamonds")
-        card2 = PlayingCard("J", "Diamonds")
-        self.assertNotEqual(card1, card2)
+
+class TestDeck(unittest.TestCase):
+    def test_deck_initialization(self):
+        deck = Deck()
+        self.assertEqual(len(deck), 52)  # Standard deck size
+        self.assertEqual(str(deck.cards[0]), "2 of Hearts")  # First card
+
+    def test_shuffle(self):
+        deck = Deck()
+        original_order = deck.cards[:]
+        deck.shuffle()
+        self.assertNotEqual(deck.cards, original_order)  # Check if deck order changed
+
+    def test_draw_cards(self):
+        deck = Deck()
+        cards_drawn = deck.draw(5)
+        self.assertEqual(len(cards_drawn), 5)
+        self.assertEqual(len(deck), 47)  # 52 - 5 cards drawn
+
+    def test_draw_too_many_cards(self):
+        deck = Deck()
+        with self.assertRaises(ValueError):
+            deck.draw(53)  # Exceeds deck size
+
+
+class TestHand(unittest.TestCase):
+    def test_draw_random_card(self):
+        deck = Deck()
+        hand = Hand()
+        hand.draw_random_card(deck)
+        self.assertEqual(len(hand), 1)
+        self.assertEqual(len(deck), 51)
+
+    def test_view_cards(self):
+        deck = Deck()
+        hand = Hand()
+        hand.draw_random_card(deck)
+        self.assertEqual(len(hand.view_cards()), 1)
+        self.assertIn(hand.view_cards()[0], [str(card) for card in deck.cards + hand.cards])
+
+    def test_hand_length(self):
+        hand = Hand()
+        self.assertEqual(len(hand), 0)
+        deck = Deck()
+        hand.draw_random_card(deck)
+        self.assertEqual(len(hand), 1)
+
 
 if __name__ == "__main__":
     unittest.main()
